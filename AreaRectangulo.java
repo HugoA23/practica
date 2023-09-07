adf199da741b1bdbab77ce1d106422e0172f588b
HugoA23 <143725565+HugoA23@users.noreply.github.com>
2023-09-07 09:48:00 -0500

    Add files via upload

---
+++ AreaRectangulo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author labcca
+ */
+public class AreaRectangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int h = 7;
+        int b = 23;
+        int area = b*h;
+        System.out.println("Hugo Andre Sanchez Zendejas");
+        System.out.println("El area de un Rectangulo es: "+ area);
+    }
+    
+}
